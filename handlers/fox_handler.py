1143ec3e6f1d87b336872ecc409e94685a9caaf3
ynthademov <suddistt@gmail.com>
2025-02-08 18:47:09 +0300

    Adds delete message and time before sending new message

--- handlers/fox_handler.py
+++ handlers/fox_handler.py
@@ -1,4 +1,6 @@
 from telethon import events
+import time
+import asyncio
 
 from my_logger.logger import log
 from utils.parser import download_image
@@ -19,6 +21,7 @@ class FoxHandler:
         self.client = client
         self.url: str = url
         self.output_file: str = output_file
+        self.last_used = 0
 
     def fox_handler(self):
         """
@@ -27,11 +30,32 @@ class FoxHandler:
         When the '/send_fox' command is received, this method downloads a fox image
         from the configured URL, saves it to the configured file name, and sends
         the image as a reply to the message. Logs the ID of the user who sent the
-        message.
+        message. Deleted message after 300 seconds.
         """
         @self.client.on(events.NewMessage(pattern=r'^\/send_fox'))
         async def send_fox(msg):
-            log.info(f"User with ID: {msg.sender_id} using fox_handler.")
-            download_image(self.url, self.output_file)
-            await msg.reply(file='image.jpg')
+            time_now = int(time.time())
 
+            log.info(f"User with ID: {msg.sender_id} using fox_handler.")
+
+            if time_now - self.last_used < 60:
+                await msg.reply('This command can be used only once per minute.')
+
+                log.error("User with ID: {} tried to use fox_handler too often.".format(msg.sender_id))
+
+                return
+            self.last_used = time_now
+            download_image(self.url, self.output_file)
+            sent_message = await msg.reply(file='image.jpg')
+
+            log.debug("Sending image with caption '{caption}'.")
+
+            msg_id = sent_message.id
+
+            log.debug("Waiting 300 seconds...")
+
+            await asyncio.sleep(300)
+            try:
+                await self.client.delete_messages(msg.chat_id, [msg_id])
+            except Exception as e:
+                log.warning(f"Failed to delete message: {e}")
